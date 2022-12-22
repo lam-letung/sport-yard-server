a5af89df856213a39f16c9d3a08edbc06e372f6c
chjcago2001 <88874679+chjcago2001@users.noreply.github.com>
2022-12-22 12:29:54 +0700

    up

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ io.on('connection', function (socket) {
               const dateNow = new Date()
                const orders_expired = orders_pending?.filter(o => {
                 const date = new Date(o.createdAt).getDate()
-                const hours = new Date(o.createdAt).getHours()
-                const mins = new Date(o.createdAt).getMinutes() 
+                const hours = new Date(o.createdAt).getHours() - 1
+                const mins = new Date(o.createdAt).getMinutes() + 3
                  return date === dateNow.getDate() && dateNow.getHours() >= hours + 1  && mins < dateNow.getMinutes()
                })
                console.log("orders_pending:",orders_pending)
